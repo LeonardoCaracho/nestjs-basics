bafedaff76d370242f64fd75dec8a90d8ef56d25
negreyscow <leeo.apc@gmail.com>
2020-06-08 07:16:04 -0300

    authorization, logging and pro-production config

--- nestjs-task-management/src/tasks/task.repository.ts
+++ nestjs-task-management/src/tasks/task.repository.ts
@@ -1,3 +1,5 @@
+import { Logger, InternalServerErrorException } from '@nestjs/common';
+import { User } from './../auth/user.entity';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { Task } from './task.entity'
@@ -6,9 +8,16 @@ import { TaskStatus } from './task-status.enum';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    private logger = new Logger()
+
+    async getTasks(
+        filterDto: GetTasksFilterDto,
+        user: User
+    ): Promise<Task[]> {
         const { status, search } = filterDto
         const query = this.createQueryBuilder('task')
+
+        query.where('task.userId = :userId', { userId: user.id })
 
         if (status){
             query.andWhere('task.status = :status', { status })
@@ -18,11 +27,20 @@ export class TaskRepository extends Repository<Task> {
             query.andWhere('task.title LIKE :search OR task.description LIKE :search', { search: `%${search}%` })
         }
 
-        const tasks = await query.getMany()
-        return tasks
+        try {
+            const tasks = await query.getMany()
+            return tasks
+        } catch (error) {
+            this.logger.error(`Failed to get tasks for user ${user.username}, Filters: ${JSON.stringify(filterDto)}`, error.stack)
+            throw new InternalServerErrorException()
+        }
+
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(
+        createTaskDto: CreateTaskDto,
+        user: User
+    ): Promise<Task> {
         const { title, description } = createTaskDto
 
         const task = new Task()
@@ -30,7 +48,16 @@ export class TaskRepository extends Repository<Task> {
         task.description = description
         task.title = title
         task.status = TaskStatus.OPEN
-        await task.save()
+        task.user = user
+
+        try {
+            await task.save()
+        } catch (error) {
+            this.logger.error(`Failed to create a task for user ${user.username}. Data: ${createTaskDto}`, error.stack)
+            throw new InternalServerErrorException()
+        }
+
+        delete task.user
 
         return task
     }
--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,44 +1,62 @@
 import { AuthGuard } from '@nestjs/passport';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { CreateTaskDto } from './dto/create-task-dto';
-import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch, Query, UsePipes, ValidationPipe, ParseIntPipe, UseGuards, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity'
+import { User } from './../auth/user.entity';
 import { TaskStatus } from './task-status.enum';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+    private logger = new Logger('TasksController')
     constructor(private tasksService: TasksService) {}
     
     @Get()
     @UsePipes(ValidationPipe)
-    getTasks(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
-        return this.tasksService.getTasks(filterDto)
+    getTasks(
+        @Query() filterDto: GetTasksFilterDto,
+        @GetUser() user: User
+    ): Promise<Task[]> {
+        this.logger.verbose(`User ${user.username} retrieving all tasks. Filters: ${JSON.stringify(filterDto)}`)
+        return this.tasksService.getTasks(filterDto, user)
     }
 
     @Get(':id')
-    getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-        return this.tasksService.getTaskById(id)
+    getTaskById(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User
+    ): Promise<Task> {
+        return this.tasksService.getTaskById(id, user)
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.tasksService.createTask(createTaskDto)
+    createTask(
+        @Body() createTaskDto: CreateTaskDto,
+        @GetUser() user: User
+    ): Promise<Task> {
+        this.logger.verbose(`User ${user.username} creating a new task. Data: ${JSON.stringify(createTaskDto)}`)
+        return this.tasksService.createTask(createTaskDto, user)
     }
 
     @Delete(':id')
-    deleteTask(@Param('id', ParseIntPipe) id: number): void{
-        this.tasksService.deleteTask(id)
+    deleteTask(
+        @Param('id', ParseIntPipe) id: number,
+        @GetUser() user: User
+    ): void {
+        this.tasksService.deleteTask(id, user)
     }
 
     @Patch(':id/status')
     updateTaskStatus(
         @Param('id', ParseIntPipe) id: number,
-        @Body('status', TaskStatusValidationPipe) status: TaskStatus 
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+        @GetUser() user: User
     ): Promise<Task> {
-        return this.tasksService.updateTaskStatus(id, status)
+        return this.tasksService.updateTaskStatus(id, status, user)
     }
 }
