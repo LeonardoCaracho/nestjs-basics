40bc2f7741ddd6eb659dc061636ec41486a8dabe
negreyscow <leeo.apc@gmail.com>
2020-06-13 18:02:18 -0300

    graphql application

--- nestjs-task-management/src/auth/jwt.strategy.spec.ts
+++ nestjs-task-management/src/auth/jwt.strategy.spec.ts
@@ -10,35 +10,35 @@ const mockUserRepository = () => ({
 });
 
 describe('JwtStrategy', () => {
-  let jwtStrategy: JwtStrategy;
-  let userRepository;
+    let jwtStrategy: JwtStrategy;
+    let userRepository;
 
-  beforeEach(async () => {
+    beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [
+        providers: [
         JwtStrategy,
         { provide: UserRepository, useFactory: mockUserRepository },
-      ],
+        ],
     }).compile();
 
     jwtStrategy = await module.get<JwtStrategy>(JwtStrategy);
     userRepository = await module.get<UserRepository>(UserRepository);
-  });
+    });
 
-  describe('validate', () => {
+    describe('validate', () => {
     it('validates and returns the user based on JWT payload', async () => {
-      const user = new User();
-      user.username = 'TestUser';
+        const user = new User();
+        user.username = 'TestUser';
 
-      userRepository.findOne.mockResolvedValue(user);
-      const result = await jwtStrategy.validate({ username: 'TestUser' });
-      expect(userRepository.findOne).toHaveBeenCalledWith({ username: 'TestUser' });
-      expect(result).toEqual(user);
+        userRepository.findOne.mockResolvedValue(user);
+        const result = await jwtStrategy.validate({ username: 'TestUser' });
+        expect(userRepository.findOne).toHaveBeenCalledWith({ username: 'TestUser' });
+        expect(result).toEqual(user);
     });
 
     it('throws an unauthorized exception as user cannot be found', () => {
-      userRepository.findOne.mockResolvedValue(null);
-      expect(jwtStrategy.validate({ username: 'TestUser' })).rejects.toThrow(UnauthorizedException);
+        userRepository.findOne.mockResolvedValue(null);
+        expect(jwtStrategy.validate({ username: 'TestUser' })).rejects.toThrow(UnauthorizedException);
     });
-  });
+    });
 })
